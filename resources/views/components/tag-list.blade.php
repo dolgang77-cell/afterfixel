6e4fdb8008f7f6e8dad8ffeb994daced36018114
dolgang77-cell <dolgang77@gmail.com>
2026-04-27 15:17:42 +0900

    Delete resources directory

--- resources/views/components/tag-list.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['tags'])
-
-@if($tags && count($tags) > 0)
-<div class="flex flex-wrap gap-1.5">
-    @foreach($tags as $tag)
-        <span class="text-[11px] font-medium bg-purple-500/[0.06] text-purple-300/80 px-2.5 py-1 rounded-lg border border-purple-500/[0.06]">#{{ $tag }}</span>
-    @endforeach
-</div>
-@endif
